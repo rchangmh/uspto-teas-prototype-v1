fac71e1366170eb23d4ded1e2a0ef7f9f7f265e1
Ronald <rchangmh@gmail.com>
2018-02-16 12:42:52 -0500

    finished form

--- src/containers/TeasForm.js
+++ src/containers/TeasForm.js
@@ -1,53 +1,93 @@
 import React, { Component } from 'react'
-import { Input, container, Form, Radio } from '/src/styles'
+import { Input, container, Form, Radio, Button } from '/src/styles'
 
 export default class TeasForm extends Component {
-	state = {
-		forms: [
-			'Owner of Mark',
-			'Internal Address',
-			'Street Address',
-			'Phone Number',
-			'Fax Number',
-			'Email Address',
-			'Website Address'
-		],
-		name: '',
-		value: ''
-	}
+  state = {
+    forms: [
+      'Owner of Mark',
+      'Internal Address',
+      'Street Address',
+      'Phone Number',
+      'Fax Number',
+      'Email Address',
+      'Website Address'
+    ],
+    representing: '',
+    representingFields: [
+      'Also known as',
+      'Trading as',
+      'Formerly',
+      'Doing business as'
+    ],
+    entityType: '',
+    entityTypes: [
+      'Individual',
+      'Corporation',
+      'LLC',
+      'Partnership',
+      'Joint Venture',
+      'Sole Proprietorship',
+      'Trust',
+      'Estate',
+      'Other'
+    ]
+  }
 
-	render() {
-		return (
-			<div style={container}>
-				{this.state.forms.map(form => (
-					<div style={{ padding: '10px' }}>
-						<Input label={form} placeholder={form} />
-					</div>
-				))}
-				<Form>
-					<Form.Field>
-						Selected value: <b>{this.state.value}</b>
-					</Form.Field>
-					<Form.Field>
-						<Radio
-							label="Choose this"
-							name="radioGroup"
-							value="this"
-							checked={this.state.value === 'this'}
-							onChange={() => this.setState({ value: 'this' })}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<Radio
-							label="Or that"
-							name="radioGroup"
-							value="that"
-							checked={this.state.value === 'that'}
-							onChange={() => this.setState({ value: 'that' })}
-						/>
-					</Form.Field>
-				</Form>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div style={container}>
+        {this.state.forms.slice(0, 1).map(form => (
+          <div style={{ padding: '10px' }} key={form}>
+            <Input label={form} placeholder={form} key={form} />
+          </div>
+        ))}
+
+        <Form>
+          <Form.Field>
+            <b>Representing: {this.state.representing}</b>
+          </Form.Field>
+          {this.state.representingFields.map(field => (
+            <Form.Field>
+              <Radio
+                key={field}
+                label={field}
+                name="representingRadio"
+                value={field}
+                checked={this.state.representing === field}
+                onChange={(e, { value }) =>
+                  this.setState({ representing: value })
+                }
+              />
+            </Form.Field>
+          ))}
+
+          <Form.Field>
+            <b>Entity Type: {this.state.entityType}</b>
+          </Form.Field>
+          {this.state.entityTypes.map(entity => (
+            <Form.Field>
+              <Radio
+                key={entity}
+                label={entity}
+                name="entityRadio"
+                value={entity}
+                checked={this.state.entityType === entity}
+                onChange={(e, { value }) =>
+                  this.setState({ entityType: value })
+                }
+              />
+            </Form.Field>
+          ))}
+        </Form>
+
+        {this.state.forms.slice(1).map(form => (
+          <div style={{ padding: '10px' }} key={form}>
+            <Input label={form} placeholder={form} key={form} />
+          </div>
+        ))}
+
+        <Button>Submit</Button>
+      </div>
+    )
+  }
 }
